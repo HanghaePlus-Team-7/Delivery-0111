5bd643f51219e0700a4fcf2864929329b89c0f81
pooreumu <pooreum83@gmail.com>
2023-06-21 21:13:08 +0900

    🧪 매장에서 처리하는 주문 관련 실패하는 e2e 테스트 추가

---
+++ test/orders/orders-store.e2e-spec.ts
@@ -0,0 +1,86 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "../../src/app.module";
+import { ShoppingBasketEntity } from "../../src/shopping-basket/shopping-basket.entity";
+import { OrdersEntity } from "../../src/orders/orders.entity";
+import { StoreEntity } from "../../src/store/store.entity";
+
+describe("주문 매장 (e2e)", () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  describe("주문 확정", () => {
+    let shoppingBasket: ShoppingBasketEntity;
+
+    beforeEach(() => {
+      // TODO: 제대로 된 샘플 데이터로 변경 필요
+      shoppingBasket = ShoppingBasketEntity.of({});
+    });
+
+    it("주문 확정 성공하면 201 응답을 보내나?", async () => {
+      const res = await request(app.getHttpServer()).post("/orders").send(shoppingBasket);
+      expect(res.status).toBe(201);
+    });
+  });
+
+  describe("주문 전체 조회", () => {
+    // TODO: 로그인 구현 후 토큰에서 매장 아이디 가져오기 url 변경 필요
+    let store: StoreEntity;
+
+    beforeEach(async () => {
+      // TODO: 제대로 된 샘플 데이터로 변경 필요
+      store = StoreEntity.of({});
+    });
+
+    it("주문 조회 성공하면 200 응답을 보내나?", async () => {
+      const res = await request(app.getHttpServer()).get(`/orders/stores/${store.id}`);
+      expect(res.status).toBe(200);
+    });
+  });
+
+  describe("주문 상세 조회", () => {
+    let orders: OrdersEntity;
+
+    beforeEach(async () => {
+      // TODO: 제대로 된 샘플 데이터로 변경 필요
+      orders = OrdersEntity.of({});
+    });
+
+    it("주문 상세 조회 성공하면 200 응답을 보내나?", async () => {
+      const res = await request(app.getHttpServer()).get(`/orders/${orders.id}`);
+      expect(res.status).toBe(200);
+    });
+
+    it("주문 상세 조회 없는 주문 아이디로 조회하면 404 응답을 보내나?", async () => {
+      orders = OrdersEntity.of({ id: "no exist uuid" });
+
+      const res = await request(app.getHttpServer()).get(`/orders/${orders.id}`);
+      expect(res.status).toBe(404);
+    });
+
+    it.todo("자신의 매장의 주문이 아닌 id로 조회하면 403 응답을 보내나? 로그인 생기고 나서 구현");
+  });
+
+  describe("주문 취소", () => {
+    let orders: OrdersEntity;
+
+    beforeEach(async () => {
+      // TODO: 제대로 된 샘플 데이터로 변경 필요
+      orders = OrdersEntity.of({});
+    });
+
+    it("주문 취소 성공하면 200 응답을 보내나?", async () => {
+      const res = await request(app.getHttpServer()).patch(`/orders/${orders.id}`);
+      expect(res.status).toBe(200);
+    });
+  });
+});
