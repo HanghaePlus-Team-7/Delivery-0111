845247beac536048cf51300367c8d72501f89a35
pooreumu <pooreum83@gmail.com>
2023-06-25 22:46:02 +0900

    ✨ add swagger api document builder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
@@ -12,6 +13,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Delivery0111 API")
+    .setDescription("Delivery0111 API description")
+    .setVersion("0.1")
+    .addTag("Delivery0111")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
