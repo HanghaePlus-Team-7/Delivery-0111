3b603c2b87de004a73eae7b7e689c17df24e3c83
waveinyu <waveinyu@gmail.com>
2023-06-26 18:00:26 +0900

    :construction: `get-all-basket` in progress

--- src/shopping-basket/shopping-basket.controller.ts
+++ src/shopping-basket/shopping-basket.controller.ts
@@ -1,13 +1,22 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
-import { AddBasketDto } from "@shopping-basket/dto/add-basket.dto";
+import { AddBasketDto, GetAllBasketDto } from "@shopping-basket/dto/shopping-basket.dto";
 import { AddBasketService } from "@shopping-basket/services/add-basket/add-basket.service";
+import { GetAllBasketService } from "@shopping-basket/services/get-all-basket/get-all-basket.service";
 
 @Controller("shopping-basket")
 export class ShoppingBasketController {
-  constructor(private readonly addBasketService: AddBasketService) {}
+  constructor(
+    private readonly addBasketService: AddBasketService,
+  ) // private readonly getAllBasketService: GetAllBasketService,
+  {}
 
   @Post()
   addBasket(@Body() addBasketDto: AddBasketDto) {
     return this.addBasketService.addBasket(addBasketDto);
   }
+
+  // @Get()
+  // getAllBasket(getAllBasketDto: GetAllBasketDto[]) {
+  //   return this.getAllBasketService.getAllBasket(getAllBasketDto);
+  // }
 }
