b51260261b39498668861aa0e57bbbd0fa535b68
waveinyu <waveinyu@gmail.com>
2023-06-26 12:02:39 +0900

    :art: Add folder directory & `shopping-basket/services`
    - 절대경로 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { OrdersModule } from "@orders/orders.module";
 
 import { ProductsModule } from "./products/products.module";
 import { ReviewsModule } from "./reviews/reviews.module";
-import { ShoppingBasketModule } from "./shopping-basket/shopping-basket.module";
+import { ShoppingBasketModule } from "@shopping-basket/shopping-basket.module";
 import { StoreModule } from "./store/store.module";
 import { UserModule } from "./user/user.module";
+import { AddBasketService } from "./shopping-basket/services/add-basket/add-basket.service";
 
 @Module({
   imports: [UserModule, StoreModule, ProductsModule, OrdersModule, ReviewsModule, ShoppingBasketModule],
   controllers: [],
-  providers: [],
+  providers: [AddBasketService],
 })
 export class AppModule {}
