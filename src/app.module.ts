6e49f9bc79e1985eafb3ef88793cf09d694cf414
waveinyu <waveinyu@gmail.com>
2023-06-26 20:44:37 +0900

    :sparkles: Create `get-all-basket`
    - `app.modules.ts` providers 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,10 @@ import { ReviewsModule } from "./reviews/reviews.module";
 import { ShoppingBasketModule } from "@shopping-basket/shopping-basket.module";
 import { StoreModule } from "./store/store.module";
 import { UserModule } from "./user/user.module";
-import { AddBasketService } from "./shopping-basket/services/add-basket/add-basket.service";
-import { GetAllBasketService } from "./shopping-basket/services/get-all-basket/get-all-basket.service";
 
 @Module({
   imports: [UserModule, StoreModule, ProductsModule, OrdersModule, ReviewsModule, ShoppingBasketModule],
   controllers: [],
-  providers: [AddBasketService, GetAllBasketService],
+  providers: [],
 })
 export class AppModule {}
--- src/shopping-basket/shopping-basket.module.ts
+++ src/shopping-basket/shopping-basket.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ShoppingBasketController } from "@shopping-basket/shopping-basket.controller";
 import { AddBasketService } from "@shopping-basket/services/add-basket/add-basket.service";
+import { GetAllBasketService } from "@shopping-basket/services/get-all-basket/get-all-basket.service";
 
 @Module({
   controllers: [ShoppingBasketController],
-  providers: [AddBasketService],
+  providers: [AddBasketService, GetAllBasketService],
 })
 export class ShoppingBasketModule {}
