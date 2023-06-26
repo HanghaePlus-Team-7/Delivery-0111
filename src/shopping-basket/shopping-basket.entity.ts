a431470874c4f4890a6c0d452ccbca7cd9f170ee
waveinyu <waveinyu@gmail.com>
2023-06-26 13:39:12 +0900

    :sparkles: Create dto(`add-basket`) and Update entitiy(`shopping-basket`)

--- src/shopping-basket/shopping-basket.entity.ts
+++ src/shopping-basket/shopping-basket.entity.ts
@@ -1,12 +1,7 @@
 export class ShoppingBasketEntity {
-  id: string;
-  userId: string;
-  storeId: string; // 해보면서 생각
-  productId: string; // 해보면서 생각
-  code: string;
-  name: string;
-  amount: number;
-  price: number;
+  id: number;
+  userId: number;
+  productId: number;
 
   constructor() {}
 
