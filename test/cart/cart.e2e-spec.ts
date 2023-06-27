11ab29ca6a2934f15024b3d9cce6287b203bf188
waveinyu <waveinyu@gmail.com>
2023-06-27 23:04:56 +0900

    :white_check_mark: Update `cart` and `orders(user)` e2e test

--- test/cart/cart.e2e-spec.ts
+++ test/cart/cart.e2e-spec.ts
@@ -5,7 +5,7 @@ import * as request from "supertest";
 
 import { AppModule } from "@root/app.module";
 
-describe("cart:e2e", () => {
+describe("장바구니 (e2e)", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -17,7 +17,7 @@ describe("cart:e2e", () => {
     await app.init();
   });
 
-  describe("장바구니 추가", () => {
+  describe("add-cart 장바구니 추가", () => {
     let cart: { id: bigint; userId: bigint; productId: bigint; amount: number };
 
     beforeEach(() => {
@@ -31,6 +31,35 @@ describe("cart:e2e", () => {
     });
   });
 
-  describe("장바구니 조회", () => {});
-  describe("장바구니 빼기", () => {});
+  describe("get-cart 장바구니 조회", () => {
+    let cart: { id: bigint; userId: bigint; productId: bigint; amount: number }[];
+
+    beforeEach(() => {
+      // TODO: 제대로 된 샘플 데이터로 변경 필요
+      cart = [
+        { id: 1n, userId: 2n, productId: 3n, amount: 5 },
+        { id: 2n, userId: 3n, productId: 1n, amount: 1 },
+        { id: 3n, userId: 1n, productId: 2n, amount: 3 },
+      ];
+    });
+
+    it("장바구니 조회를 성공하면 200 응답을 보낸다", async () => {
+      const res = await request(app.getHttpServer()).get(`/cart`);
+      expect(res.status).toBe(200);
+    });
+  });
+
+  describe("delete-cart 장바구니 빼기", () => {
+    let cart: { id: bigint; userId: bigint; productId: bigint; amount: number };
+
+    beforeEach(() => {
+      // TODO: 제대로 된 샘플 데이터로 변경 필요
+      cart = { id: 1n, userId: 2n, productId: 3n, amount: 5 };
+    });
+
+    it("장바구니 빼기를 성공하면 200 응답을 보낸다", async () => {
+      const res = await request(app.getHttpServer()).get(`/cart`);
+      expect(res.status).toBe(204);
+    });
+  });
 });
