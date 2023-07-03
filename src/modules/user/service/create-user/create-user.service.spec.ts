25336a963e92695b7d72b9a10df064219ae733e2
이현규 <aiden@guarantable.com>
2023-07-03 17:27:57 +0900

    ⚡️ user 단위테스트 실행 실패 원인 파악 요청

--- src/modules/user/service/create-user/create-user.service.spec.ts
+++ src/modules/user/service/create-user/create-user.service.spec.ts
@@ -1,13 +1,46 @@
-import { Injectable } from "@nestjs/common";
-import { CreateUser } from "./create-user.interface";
+import { CREATE_USER } from "./create-user.interface";
 import { UserRepository } from "../../user.repository";
 import { CreateUserDto } from "../../dto/request/create-user.dto";
+import { Test, TestingModule } from "@nestjs/testing";
+import { CreateUserService } from "./create-user.service";
 
-// @Injectable()
-// export class CreateUSerService implements CreateUser {
-//   constructor(private readonly userRepository: UserRepository) {}
+jest.mock("@user/user.repository");
 
-//   async execute(createUserDto: CreateUserDto): Promise<CreateUserDto> {
-//     return await this.userRepository.createUser(createUserDto.toEntity());
-//   }
-// }
+describe("CreateUser", () => {
+  let creaUserService: CreateUserService;
+  let userRepository: UserRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: CREATE_USER,
+          useClass: CreateUserService,
+        },
+        UserRepository,
+      ],
+    }).compile();
+
+    creaUserService = module.get<CreateUserService>(CREATE_USER);
+    userRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  it("CreateUser 인자로 createUserService.execute를 실행하면 mockCreateUserDto.toEntity()를 인자로 createUser 실행하나?", async () => {
+    const mockCreateUserDto = CreateUserDto.of({
+      nickname: "test",
+      email: "asdf1231421asdl@gmail.com",
+      password: "asdf1234!@#",
+      phone: "01012341234",
+      address: "서울시 강남구",
+    });
+
+    const res = await creaUserService.execute(mockCreateUserDto);
+    console.log("res ---------------", res);
+
+    expect(res).not.toBe(undefined);
+    // expect(res).toBe(201);
+    // expect(userRepository.createUser(mockCreateUserDto)).toBeCalledTimes(1);
+    expect(userRepository.createUser(mockCreateUserDto)).toBe(undefined);
+    expect(userRepository.createUser(mockCreateUserDto)).toBeCalledWith(mockCreateUserDto.toEntity());
+  });
+});
