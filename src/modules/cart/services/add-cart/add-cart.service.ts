b34286e4472750e5f59d234264e99c07786ffb2e
waveinyu <waveinyu@gmail.com>
2023-07-03 15:59:16 +0900

    :art: Update `add-cart.service`

--- src/modules/cart/services/add-cart/add-cart.service.ts
+++ src/modules/cart/services/add-cart/add-cart.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from "@nestjs/common";
 
-import { AddCartDto } from "@cart/dto/cart.dto";
+import { CartRepository } from "@cart/cart.repository";
+import { AddCartDto } from "@cart/dto/request/add-cart.dto";
+import { AddCart } from "@cart/services/add-cart/add-cart.interface";
 
+jest.mock("@cart/cart.repository");
+
+// @Injectable()
+// export class AddCartService {
+//   addBasket(addCartDto: AddCartDto) {
+//     return "This action adds product in shopping basket";
+//   }
+// }
 @Injectable()
-export class AddCartService {
-  addBasket(addCartDto: AddCartDto) {
-    return "This action adds product in shopping basket";
+export class AddCartService implements AddCart {
+  constructor(private readonly cartRepository: CartRepository) {}
+
+  async execute(addCartDto: AddCartDto): Promise<void> {
+    return await this.cartRepository.addCart(addCartDto);
   }
+
+  // addBasket(addCartDto: AddCartDto) {
+  //   return "This action adds product in shopping basket";
+  // }
 }
