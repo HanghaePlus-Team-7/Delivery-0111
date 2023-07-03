47681b32c21b609eec41942ae07be4affb3a23e7
이현규 <aiden@guarantable.com>
2023-07-03 14:13:35 +0900

    ⚡️ create-user 부분 분리, 폴더구조 변경

--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,7 +1,7 @@
 import { ConflictException, HttpException, Injectable } from "@nestjs/common";
 
-import { CreateUserDto } from "@user/dto/create-user.dto";
-import { UserEntity } from "@user/user.entity";
+import { CreateUserDto } from "@root/modules/user/dto/request/create-user.dto";
+import { UserEntity } from "@root/modules/user/entities/user.entity";
 import { PrismaService } from "@root/prisma/prisma.service";
 import { UserRepository } from "@user/user.repository";
 
@@ -26,7 +26,14 @@ export class UserService {
     const { password, id: userId, ...result } = found;
     return result;
   }
-
+  async findEmail(email: string) {
+    const found = await this.userRepository.findEmail(email);
+    if (!found) {
+      throw new HttpException(`there is no ${email}`, 400);
+    }
+    // const { password, id: userId, ...result } = found;
+    return found;
+  }
   // deleteUser(id: string) {
   //   this.users = this.users.filter((user) => user.id !== id);
   // }
