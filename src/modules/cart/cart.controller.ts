88afc3b008339b45db563d250d02eb30a416f7c0
waveinyu <waveinyu@gmail.com>
2023-07-03 16:49:19 +0900

    :art: Update `src/cart` & Create `cart.repository.spec.ts`

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 
-import { AddCartDto, GetAllCartDto } from "@root/modules/cart/dto/request/add-cart.dto";
+import { AddCartDto } from "@cart/dto/request/add-cart.dto";
 import { AddCartService } from "@cart/services/add-cart/add-cart.service";
 import { GetAllCartService } from "@cart/services/get-all-cart/get-all-cart.service";
 
@@ -9,12 +9,12 @@ export class CartController {
   constructor(private readonly addCartService: AddCartService, private readonly getAllCartService: GetAllCartService) {}
 
   @Post()
-  addBasket(@Body() addCartDto: AddCartDto) {
-    return this.addCartService.addBasket(addCartDto);
+  addCart(@Body() addCartDto: AddCartDto) {
+    return this.addCartService.execute(addCartDto);
   }
 
-  @Get()
-  getAllBasket(getAllCart: GetAllCartDto) {
-    return this.getAllCartService.getAllBasket(getAllCart);
-  }
+  // @Get()
+  // getAllBasket(getAllCart: GetAllCartDto) {
+  //   return this.getAllCartService.getAllBasket(getAllCart);
+  // }
 }
