fbf5fb667693b4e6e1fbb8b4a758adfc7512584a
pooreumu <pooreum83@gmail.com>
2023-07-16 18:11:07 +0900

    ✨ add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+
+import { LoggerMiddleware } from "@root/middleware/logger.middleware";
+import { WinstonLogger } from "@root/middleware/winston-logger";
 
 import { CartModule } from "@cart/cart.module";
 
@@ -11,11 +14,23 @@ import { ReviewsModule } from "@reviews/reviews.module";
 import { StoreModule } from "@store/store.module";
 
 import { UserModule } from "@user/user.module";
+
 import { AuthModule } from "./modules/auth/auth.module";
 
 @Module({
-  imports: [UserModule, StoreModule, ProductsModule, OrdersModule, ReviewsModule, CartModule, AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    UserModule,
+    StoreModule,
+    ProductsModule,
+    OrdersModule,
+    ReviewsModule,
+    CartModule,
+    AuthModule,
+    new WinstonLogger().getRequestLogger(),
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes("*");
+  }
+}
