84a5c5332148c2f0013b36382d39af4993deeb69
pooreumu <pooreum83@gmail.com>
2023-07-20 14:35:30 +0900

    ✨ 이미지 파일 저장하는 기능 추가

---
+++ src/middleware/multer/multer-config.service.ts
@@ -0,0 +1,31 @@
+import * as fs from "fs";
+import * as process from "process";
+
+import { Injectable } from "@nestjs/common";
+import { MulterModuleOptions, MulterOptionsFactory } from "@nestjs/platform-express";
+
+import { diskStorage } from "multer";
+
+@Injectable()
+export class MulterConfigService implements MulterOptionsFactory {
+  constructor() {}
+
+  createMulterOptions(): Promise<MulterModuleOptions> | MulterModuleOptions {
+    const filePath = process.env.UPLOAD_FILE_PATH || "@root/../test/data/uploads";
+    const fileName = process.env.UPLOAD_FILE_NAME || "test.png";
+    return {
+      storage: diskStorage({
+        destination: (req, file, cb) => {
+          if (!fs.existsSync(filePath)) {
+            fs.mkdirSync(filePath, { recursive: true });
+          }
+
+          cb(null, filePath);
+        },
+        filename: (req, file, cb) => {
+          cb(null, fileName);
+        },
+      }),
+    };
+  }
+}
