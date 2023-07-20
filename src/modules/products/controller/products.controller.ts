4bbd36eb03d37a8a6d420af99c3790b205101c1d
pooreumu <pooreum83@gmail.com>
2023-07-20 20:47:49 +0900

    ✨ Implement add product controller

--- src/modules/products/controller/products.controller.ts
+++ src/modules/products/controller/products.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
-import { FilesInterceptor } from "@nestjs/platform-express";
+import { Body, Controller, Inject, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+
+import { AddProductRequest } from "@products/controller/dto/add-product.request";
+import { PRODUCT_SERVICE, ProductService } from "@products/service/product.service";
 
 @Controller("products")
 export class ProductsController {
-  constructor() {}
+  constructor(@Inject(PRODUCT_SERVICE) private readonly productService: ProductService) {}
 
   @Post()
-  @UseInterceptors(FilesInterceptor("photo"))
-  async addProduct(@UploadedFiles() photo: Express.Multer.File) {
-    throw new Error("Not Implemented");
+  @UseInterceptors(FileInterceptor("photo"))
+  async addProduct(@UploadedFile() photo: Express.Multer.File, @Body() addProductRequest: AddProductRequest) {
+    await this.productService.addProduct(addProductRequest.toCommand(photo));
   }
 }
