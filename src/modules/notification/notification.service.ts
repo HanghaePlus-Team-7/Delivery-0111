7b954f6804dc0a768652968051c9717896ed17a6
pooreumu <pooreum83@gmail.com>
2023-07-20 10:35:56 +0900

    🚚 알림보내기 service -> usecase로 변경

--- src/modules/notification/notification.service.ts
+++
@@ -1,7 +0,0 @@
-import { NotificationParam } from "@notification/notification-param";
-
-export interface NotificationService {
-  sendNotification: (params: NotificationParam) => Promise<void>;
-}
-
-export const NOTIFICATION_SERVICE = Symbol("NOTIFICATION_SERVICE");
--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -4,15 +4,14 @@ import { NotificationModule } from "@root/modules/notification/notification.modu
 import { PrismaModule } from "@root/prisma/prisma.module";
 
 import { OrdersController } from "@orders/controller/orders.controller";
+import { OrdersPrismaRepository } from "@orders/repository/orders.prisma-repository";
 import { ORDERS_REPOSITORY } from "@orders/repository/orders.repository";
 import { ORDERS_SERVICE } from "@orders/service/orders.service";
+import { OrdersServiceImpl } from "@orders/service/orders.service-impl";
 import { CONFIRM_ORDER } from "@orders/usecase/confirm-order/confirm-order";
 import { ConfirmOrderImpl } from "@orders/usecase/confirm-order/confirm-order-impl";
 import { GET_ORDERS_OF_STORE } from "@orders/usecase/get-orders-of-store/get-orders-of-store";
-
-import { OrdersPrismaRepository } from "./repository/orders.prisma-repository";
-import { OrdersServiceImpl } from "./service/orders.service-impl";
-import { GetOrdersOfStoreImpl } from "./usecase/get-orders-of-store/get-orders-of-store-impl";
+import { GetOrdersOfStoreImpl } from "@orders/usecase/get-orders-of-store/get-orders-of-store-impl";
 
 @Module({
   imports: [PrismaModule, NotificationModule],
@@ -26,8 +25,14 @@ import { GetOrdersOfStoreImpl } from "./usecase/get-orders-of-store/get-orders-o
       provide: GET_ORDERS_OF_STORE,
       useClass: GetOrdersOfStoreImpl,
     },
-    { provide: ORDERS_REPOSITORY, useClass: OrdersPrismaRepository },
-    { provide: ORDERS_SERVICE, useClass: OrdersServiceImpl },
+    {
+      provide: ORDERS_REPOSITORY,
+      useClass: OrdersPrismaRepository,
+    },
+    {
+      provide: ORDERS_SERVICE,
+      useClass: OrdersServiceImpl,
+    },
   ],
 })
 export class OrdersModule {}
