69ce0f2838b30c56323726b225b473b564ac0aea
pooreumu <pooreum83@gmail.com>
2023-07-23 22:40:30 +0900

    ✅ add unit test

---
+++ src/modules/product/controller/dto/add-product.request.spec.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidV4 } from "uuid";
+
+import { AddProductRequest } from "@product/controller/dto/add-product.request";
+import { AddProductCommand } from "@product/service/dto/add-product.command";
+
+describe("AddProductRequest", () => {
+  it("toCommand", () => {
+    const name = "name";
+    const price = 1;
+    const description = "description";
+    const storeId = uuidV4();
+
+    const addProductRequest = new AddProductRequest({ name, price, description, storeId });
+
+    const file = { path: "path" } as Express.Multer.File;
+    const addProductCommand = addProductRequest.toCommand(file);
+
+    expect(addProductCommand).toBeInstanceOf(AddProductCommand);
+    expect(addProductCommand.name).toBe(name);
+    expect(addProductCommand.price).toBe(price);
+    expect(addProductCommand.description).toBe(description);
+    expect(addProductCommand.storeId).toBe(storeId);
+    expect(addProductCommand.photoPath).toBe(file.path);
+  });
+});
--- src/modules/product/usecase/add-product/add-product.spec.ts
+++ src/modules/product/usecase/add-product/add-product.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from "@nestjs/testing";
 
+import { v4 as uuidV4 } from "uuid";
+
+import { ProductEntity } from "@product/entity/product.entity";
 import { ProductPrismaRepository } from "@product/repository/product.prisma-repository";
-import { PRODUCT_REPOSITORY } from "@product/repository/product.repository";
+import { PRODUCT_REPOSITORY, ProductRepository } from "@product/repository/product.repository";
 import { AddProduct } from "@product/usecase/add-product/add-product";
 
 jest.mock("@product/repository/product.prisma-repository");
+
 describe("AddProduct", () => {
-  let provider: AddProduct;
+  let addProduct: AddProduct;
+  let productRepository: ProductRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,10 +24,37 @@ describe("AddProduct", () => {
       ],
     }).compile();
 
-    provider = module.get<AddProduct>(AddProduct);
+    addProduct = module.get<AddProduct>(AddProduct);
+    productRepository = module.get<ProductRepository>(PRODUCT_REPOSITORY);
   });
 
-  it("should be defined", () => {
-    expect(provider).toBeDefined();
+  describe("addProduct", () => {
+    it("제대로 된 argument 로 실행함?", async () => {
+      const id = uuidV4();
+      const storeId = uuidV4();
+      const name = "name";
+      const price = 1;
+      const description = "description";
+      const photoPath = "photoPath";
+
+      const productEntity = new ProductEntity({
+        id,
+        storeId,
+        name,
+        price,
+        description,
+        photoPath,
+      });
+      await addProduct.execute(productEntity);
+
+      expect(productRepository.addProduct).toBeCalledWith({
+        id: productEntity.id,
+        storeId: productEntity.storeId,
+        name: productEntity.name,
+        price: productEntity.price,
+        description: productEntity.description,
+        image: productEntity.photoPath,
+      });
+    });
   });
 });
