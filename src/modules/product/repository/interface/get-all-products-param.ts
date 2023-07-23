5c0cb030500737e0ba025b2d62d8a129f2b47a09
waveinyu <waveinyu@gmail.com>
2023-07-23 23:05:00 +0900

    :construction: `getAllProducts` in progerss"

---
+++ src/modules/product/repository/interface/get-all-products-param.ts
@@ -0,0 +1,3 @@
+export interface GetAllProductsParam {
+  id: string;
+}
--- src/modules/product/repository/product.repository.ts
+++ src/modules/product/repository/product.repository.ts
@@ -1,7 +1,15 @@
 import { AddProductParam } from "@product/repository/interface/add-product-param";
 
+import { ProductEntity } from "../entity/product.entity";
+
+import { GetAllProductsParam } from "./interface/get-all-products-param";
+
 export interface ProductRepository {
   addProduct: (product: AddProductParam) => Promise<void>;
+  getAllProducts: (id: GetAllProductsParam) => Promise<ProductEntity[]>;
+  // getOneProduct
+  // updateProduct
+  // deleteProduct
 }
 
 export const PRODUCT_REPOSITORY = Symbol("PRODUCT_REPOSITORY");
