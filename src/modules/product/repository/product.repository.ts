af7dcad5919edd3ab49a6f1d67ba5bf4e4e51d6d
waveinyu <waveinyu@gmail.com>
2023-07-23 22:49:15 +0900

    :construction: `getAllProducts` 작업 중

--- src/modules/product/repository/product.repository.ts
+++ src/modules/product/repository/product.repository.ts
@@ -1,5 +1,11 @@
+import { Product } from "@prisma/client";
+
 export interface ProductRepository {
   addProduct: (product: any) => Promise<void>;
+  getAllProducts: (id: string) => Promise<Product[]>;
+  // getOneProduct
+  // updateProduct
+  // deleteProduct
 }
 
 export const PRODUCT_REPOSITORY = Symbol("PRODUCT_REPOSITORY");
